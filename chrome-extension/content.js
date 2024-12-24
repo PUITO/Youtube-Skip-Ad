043b2c7fbfce1dc121ad131edb17fa376d7b8d1a
hz_gaolongfei <2904340675@qq.com>
2024-12-24 22:28:32 +0800

    更新代码

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,9 +1,9 @@
 /*
  * @Author: puito123
  * @Date: 2024-12-22 13:45:58
- * @LastEditTime: 2024-12-24 17:09:54
+ * @LastEditTime: 2024-12-24 22:23:48
  * @LastEditors: puito123
- * @FilePath: \youtube\content.js
+ * @FilePath: \youtube\chrome-extension\content.js
  * @Description: 
  */
 console.log('Content script loaded');
@@ -11,7 +11,7 @@ console.log('Content script loaded');
 let intervalId = null;
 
 let lastSendTime = 0; //最后发送时间
-const sendInterval = 2000; // 发送间隔
+const sendInterval = 1000; // 发送间隔
 const maxSendsPerMinute = 5; // 最大发送次数
 let sendCount = 0; // 发送次数
 const resetInterval = 60000; // 1分钟
@@ -31,6 +31,36 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
+// 在页面加载后进行操作
+window.addEventListener('load', function () {
+  chrome.storage.sync.get(['autoSkip'], function (result) {
+    if (result.autoSkip) {
+      startAutoClick();
+    }
+  });
+
+  // // 查找所有的 <script> 标签
+  // const scripts = document.getElementsByTagName('script');
+
+  // for (let i = 0; i < scripts.length; i++) {
+  //   const script = scripts[i];
+
+  //   // 检查当前 <script> 标签的 src 属性
+  //   if (script.src.includes("base.js")) {
+  //     // 创建一个新的 <script> 标签
+  //     const newScript = document.createElement('script');
+  //     newScript.src = "base.js"; // 你的新脚本路径
+  //     newScript.nonce = ""; // 如果需要，可以设置 nonce
+  //     newScript.className = "js-httpswwwyoutubecomsplayerb46bb280player_iasvflsetzh_CNbasejs"; // 设置类名
+
+  //     // 替换旧的 <script> 标签
+  //     script.parentNode.replaceChild(newScript, script);
+
+  //     console.log('Replaced script:', script.src, 'with', newScript.src);
+  //   }
+  // }
+});
+
 
 
 function startAutoClick() {
@@ -39,7 +69,7 @@ function startAutoClick() {
     console.log('Auto click is already running.');
     return;
   }
-  intervalId = setInterval(getSkipBtn, 2000); // 每2秒检查一次
+  intervalId = setInterval(getSkipBtn, 1000); // 每2秒检查一次
 }
 
 function stopAutoClick() {
@@ -63,7 +93,7 @@ function getSkipBtn() {
     const title = fullscreenDOM.title || '';
     isVideoFullScreen = title.includes("退出全屏"); // 或者使用 indexOf
   }
-
+  // console.log(`浏览器全屏状态：${isFullScreen} 播放器全屏状态：${isVideoFullScreen}`)
   Array.from(document.getElementsByClassName("ytp-skip-ad-button")).forEach(btn => {
     if (btn.style.display == "none") {
       console.log('Button is hidden, showing it...');
@@ -100,28 +130,3 @@ setInterval(() => {
 }, resetInterval);
 
 
-/*
-// 在页面加载后进行操作
-window.addEventListener('load', function () {
-  // 查找所有的 <script> 标签
-  const scripts = document.getElementsByTagName('script');
-
-  for (let i = 0; i < scripts.length; i++) {
-    const script = scripts[i];
-
-    // 检查当前 <script> 标签的 src 属性
-    if (script.src.includes("base.js")) {
-      // 创建一个新的 <script> 标签
-      const newScript = document.createElement('script');
-      newScript.src = "base.js"; // 你的新脚本路径
-      newScript.nonce = ""; // 如果需要，可以设置 nonce
-      newScript.className = "js-httpswwwyoutubecomsplayerb46bb280player_iasvflsetzh_CNbasejs"; // 设置类名
-
-      // 替换旧的 <script> 标签
-      script.parentNode.replaceChild(newScript, script);
-
-      console.log('Replaced script:', script.src, 'with', newScript.src);
-    }
-  }
-});
-*/
